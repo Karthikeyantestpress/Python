9c401e98416592202a3c3bc79ea07b8dd16bc5f7
Karthikeyantestpress <karthikeyan@testpress.in>
2022-08-29 13:02:44 +0530

    Default value addigned to argymenets of the functioon

--- Functions.py
+++ Functions.py
@@ -1,4 +1,4 @@
-def Addition (x,y):
+def Addition (x=0,y=0):
     result = x +y
     print(result)
     return result
@@ -7,4 +7,5 @@ n2=5
 Addition(1,2)
 Addition(3.0,5.0)
 result = Addition(n1,n2)
+Addition(12)
 print(result)
