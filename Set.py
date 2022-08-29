9e3ad9567c88126919af96330f5bb295aea62882
Karthikeyantestpress <karthikeyan@testpress.in>
2022-08-29 15:24:47 +0530

    Updated set with intersection

--- Set.py
+++ Set.py
@@ -1,3 +1,8 @@
 #Set donot allow Duplicates  
 Animals ={"Dog","Cat","Tiger","Lion","Dog"}
 print(Animals)
+Animals.add("Monkeys")
+print(Animals)
+List_Animals =["Elephant","Cat","Horse"]
+Animals.update(List_Animals,{"Crocodile"})
+print(Animals)
