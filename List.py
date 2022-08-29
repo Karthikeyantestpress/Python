75e3c52d49e0b5c9a36aca8e4165ac84ff35e8c4
Karthikeyantestpress <karthikeyan@testpress.in>
2022-08-29 14:21:09 +0530

    List has been updated with new comments

--- List.py
+++ List.py
@@ -3,7 +3,7 @@ print(Languages)
 print(Languages[0])
 print(len(Languages))
 print(Languages[-1])
-Languages[0] = "React"
+Languages.append("React")
 print(Languages)
 print(Languages[0:2])
 print("Angular" in Languages)
