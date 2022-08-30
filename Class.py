78dbe5e1e812b7be66c9143ba1827a95990e1597
Karthikeyantestpress <karthikeyan@testpress.in>
2022-08-30 11:40:49 +0530

    Changes in Class has been made

--- Class.py
+++ Class.py
@@ -1,10 +1,30 @@
-class Developer:
- 
-  def __init__(self,name,platform,mark):
-     self.name=name
-     self.platform=platform
-     self.mark=mark
+class Developer():
 
-Developer2=Developer("Bala","PHP",95)
-print(Developer2.name)
+  def Check_Marks(self):
+    if self.marks>=50:
+       return True 
+    else:
+       return False 
+  
+  def Job(self):
+     
+    print("Working in Testpress")
+  
+  def __init__(self,Name,marks):
+    self.Name =Name
+    self.marks=marks
+    print(self.Name)
+    print(self.marks)
 
+Developer1=Developer()
+Developer1.Name ="Santhosh"
+Developer1.marks= 50
+
+Checking =Developer1.Check_Marks()
+print(Checking)
+print(Developer1.Name)
+
+Carrer =Developer1.Job()
+
+Developer2 =Developer("Lokesh",90)
+Developer3 =Developer("Prasanna",100)
