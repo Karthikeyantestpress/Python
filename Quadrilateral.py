d2c411f81ff060919fd03de093e4d69ace0005a3
Karthikeyantestpress <karthikeyan@testpress.in>
2022-08-30 15:33:37 +0530

    Updated

--- Quadrilateral.py
+++ Quadrilateral.py
@@ -7,6 +7,6 @@ class Quadrilateral(polygon):
    def Display(self):
         print("We are in Quadrilateral")
 
-t1=Triangle(5,6,7)
+t1=Triangle([5,6,7])
 perimeter=t1.get_perimeter()
 print(perimeter)
--- polygon.py
+++ polygon.py
@@ -7,17 +7,3 @@ class polygon:
         perimeter =sum(self.sides)
         return perimeter
 
-class Triangle(polygon):
-
-    def Display(self):
-        print("A Triangle is a polygon with edges"
-        )
-
-class Quadrilateral(polygon):
-
-   def Display(self):
-        print("We are in Quadrilateral")
-
-t1=Triangle([5,6,7])
-perimeter=t1.get_perimeter()
-print(perimeter)
