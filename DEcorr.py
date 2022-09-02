403402c3abe63078c8868d7c8705f622d375fe2c
Karthikeyantestpress <karthikeyan@testpress.in>
2022-09-02 17:16:22 +0530

    Git has been updated

--- DEcorr.py
+++ DEcorr.py
@@ -1,13 +1,8 @@
-def sum (a,b):
-  print(a+b)
+def sqrt(exponent):
+    def another(base):
+        return pow(base,exponent)
+    return another
 
-def Work(func):
-    def inner (x,y):
-        if x>0 or y>0:
-            x=x+1
-            y=y+1
-        return func(x,y)
-    return inner
-
-Calling =Work(sum)
-Calling(2,3)    
+Check=sqrt(2)
+print(Check(3))
+print(Check(4))
---
+++ Lmada.py
@@ -0,0 +1,5 @@
+double= lambda n : n*2
+print(double(5))
+
+larger =lambda a,b : a if a>b else b 
+print(larger(100,20))
