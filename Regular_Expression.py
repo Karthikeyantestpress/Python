4ada84003afa610ad5484160214ae2ee7a71a419
Karthikeyantestpress <karthikeyan@testpress.in>
2022-09-07 18:41:49 +0530

    RE updatedd

--- Regular_Expression.py
+++ Regular_Expression.py
@@ -1,31 +1,31 @@
 
 import re                                                                             
 
-# # String ="I am in bRoad  Road Roadway in "                                                              
+String ="I am in bRoad  Road Roadway in "                                                              
 
-# # Updated =re.sub(r'\bRoad\b','Testpress',String)                                         
-# # print (Updated)                                                              
+Updated =re.sub(r'\bRoad\b','Testpress',String)                                         
+print (Updated)                                                              
 
-# # # L1=[1,2,3,4,5]
-# # # L3=[v.split('2') for v in L1]
+L1=[1,2,3,4,5]
+L3={v:v for v in L1}
 
-# # # print(L3)
+print(L3)
 
-# # # L2=dict(L1)
-# # # print(L2)
+L2=dict(L1)
+print(L2)
 
-# Phone_Pattern ='''Phone Number Pattern using 
-#   regular Expression
+Phone_Pattern ='''Phone Number Pattern using 
+  regular Expression
   
-#   232-123-1231
-#   2334-30-313123
+  232-123-1231
+  2334-30-313123
 
-#   '''
+  '''
 
-# pattern= re.compile(r'[0-9]{4,5}+?-[0-9]+?-[0-9]+')
-# matches =pattern.finditer(Phone_Pattern)
-# for match in matches : 
-#     print(match)
+pattern= re.compile(r'[0-9]{4,5}+?-[0-9]+?-[0-9]+')
+matches =pattern.finditer(Phone_Pattern)
+for match in matches : 
+    print(match)
 
 
 line = "Cats are smarter than dogs";
